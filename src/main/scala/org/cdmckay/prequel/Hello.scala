016eb54fbe9bd9bd5d25786e90ef1bd8f709d642
Cameron McKay <me@cdmckay.org>
2018-04-03 12:09:55 +0200

    Add insert that makes sure rows conform to a schema

--- src/main/scala/org/cdmckay/prequel/Hello.scala
+++ src/main/scala/org/cdmckay/prequel/Hello.scala
@@ -2,8 +2,16 @@ package org.cdmckay.prequel
 
 object Hello extends App {
 
-  val table = new Table[(Int, String)]
+  val table = new Table(Array(classOf[Integer], classOf[String]))
+  table.insert(1, "one", 3)
+  table.insert("one", 1)
+
   table.insert(1, "one")
+  table.insert(2, "one")
+  table.insert(3, "three")
+
   table.selectAll()
+
+  table.selectWhereEq(1, "one")
 
 }
--- src/main/scala/org/cdmckay/prequel/Table.scala
+++ src/main/scala/org/cdmckay/prequel/Table.scala
@@ -2,12 +2,42 @@ package org.cdmckay.prequel
 
 import scala.collection.JavaConverters._
 
-class Table[A] {
+class Table(private val schema: Array[Class[_]]) {
 
-  private val rows = new java.util.LinkedList[A]()
+  type R = Array[Any]
 
-  def insert(row: A): Unit = rows.add(row)
+  private val rows = new java.util.LinkedList[R]()
 
-  def selectAll(): Unit = rows.asScala.foreach(println)
+  def insert(row: Any*): Unit = {
+    if (doesConformToSchema(row)) {
+      println("1 row inserted")
+      rows.add(row.toArray)
+    }
+  }
+
+  def selectAll(): Unit = rows.asScala.foreach(r => println(convertRowToString(r)))
+
+  def selectWhereEq(i: Int, value: Any): Unit =
+    rows.asScala.foreach { r =>
+      if (r(i) == value) {
+        println(convertRowToString(r))
+      }
+    }
+
+  private def doesConformToSchema(row: Seq[Any]): Boolean = {
+    if (row.length != schema.length) {
+      println("row has different number of columns than schema")
+      return false
+    }
+    val columnMismatches = (schema zip row).filterNot { case (s, r) => s == r.getClass }
+    if (columnMismatches.nonEmpty) {
+      println("row does not match schema: ")
+      columnMismatches.foreach { case (s, r) => println(s"  $r is a ${r.getClass} but needs to be a $s")}
+      return false
+    }
+    true
+  }
+
+  private def convertRowToString(row: R): String = row.mkString(", ")
 
 }
