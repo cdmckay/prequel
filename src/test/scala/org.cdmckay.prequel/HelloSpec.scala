51189169e76ea2cdac45dc63b0b4318070a6df1e
Cameron McKay <me@cdmckay.org>
2018-04-03 11:05:14 +0200

    Add initial files

---
+++ src/test/scala/org.cdmckay.prequel/HelloSpec.scala
@@ -0,0 +1,9 @@
+package org.cdmckay.prequel
+
+import org.scalatest._
+
+class HelloSpec extends FlatSpec with Matchers {
+  "The Hello object" should "say hello" in {
+    Hello.greeting shouldEqual "hello"
+  }
+}
